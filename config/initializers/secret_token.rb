1c014fe56a3e7d622d212f104f8673729469a2cd
asyed <asyed@lbl.gov>
2012-05-10 09:42:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChapthreeApp::Application.config.secret_token = 'f20cb4cf44052fb1b9cc78e8c5d150621e212cd1e1641ed6b29bab26cef2a8b8e93a385a660f4d8ca9eabcae81e298e2d1fbdce64628dd1724d9308fb05d222e'
